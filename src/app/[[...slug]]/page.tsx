136f6bb4fa672392d76735d950f4dd61ad92c30e
Dreta <32196824+Dreta@users.noreply.github.com>
2025-10-02 20:09:45 +0800

    feat: set correct description

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -29,19 +29,25 @@ export async function generateMetadata({ params }: {
     const language = route[0]
     const newRoute = route.slice(1)
     const slug = newRoute.length === 5 ? newRoute[4] : (newRoute.length === 0 ? '/' : newRoute.join('/'))
+    const baseDescription = language === 'en'
+        ? 'Beijing Academy International Division (BAID) is a CIS-member international high school program in Beijing offering AP and Cambridge curricula.'
+        : '北京中学国际部 (BAID) 是北京中学下设的国际高中项目，提供 AP 和剑桥课程。我们以立足北京、植根中国、面向世界的现代教育，培养全面发展的未来领袖。'
     if (slug === '/') {
         return {
-            title: language === 'en' ? 'Beijing Academy International Division' : '北京中学国际部'
+            title: language === 'en' ? 'Beijing Academy International Division' : '北京中学国际部',
+            description: baseDescription
         }
     }
     const entity = await getContentEntityBySlug(slug)
     if (entity == null) {
         return {
-            title: language === 'en' ? 'Not Found | Beijing Academy International Division' : '未找到 | 北京中学国际部'
+            title: language === 'en' ? 'Not Found | Beijing Academy International Division' : '未找到 | 北京中学国际部',
+            description: baseDescription
         }
     }
     return {
-        title: language === 'en' ? `${entity.titlePublishedEN} | Beijing Academy International Division` : `${entity.titlePublishedZH} | 北京中学国际部`
+        title: language === 'en' ? `${entity.titlePublishedEN} | Beijing Academy International Division` : `${entity.titlePublishedZH} | 北京中学国际部`,
+        description: (language === 'en' ? entity.shortContentPublishedEN : entity.shortContentPublishedZH) ?? baseDescription
     }
 }
 
