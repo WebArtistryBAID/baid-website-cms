acf0219bc461f05bf9cd67682c6920435c45f396
Dreta <32196824+Dreta@users.noreply.github.com>
2025-10-02 14:17:04 +0800

    chore: add migrations

---
+++ prisma/migrations/20251002060759_add_short_content/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "public"."ContentEntity"
+    ADD COLUMN "shortContentDraftEN" TEXT,
+ADD COLUMN     "shortContentDraftZH" TEXT,
+ADD COLUMN     "shortContentPublishedEN" TEXT,
+ADD COLUMN     "shortContentPublishedZH" TEXT;
